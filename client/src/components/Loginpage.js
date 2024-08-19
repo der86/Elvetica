10006a63bc90d04f36dfcf2976cae41c007eed17
derick <derickoj4@gmail.com>
2024-08-19 16:21:04 +0300

    added new files

---
+++ client/src/components/Loginpage.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const loginPage = () => {
+  return (
+    <div>
+        <form action="">
+            <div className='input-box'>
+                <input type="text"
+                placeholder='Username'
+                required />
+            </div>
+            <div className='input-box'>
+                <input type="password"
+                placeholder='Password'
+                required />
+            </div>
+            <div className='remember-forgot'>
+                <label><input type='checkbox'/>Remember me</label>
+                {/* <a href=''>Forgot password?</a> */}
+            </div>
+            <button type='submit'>Login</button>
+            <div className='register-link'>
+                <p>Don't have an account? <a href=''>Register</a></p>
+            </div>
+
+        </form>
+    </div>
+  )
+}
+
+export default loginPage
